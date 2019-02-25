658af8fb431f830e8482c83db15ddabbdc2038d7
Jeremy Ahn <me@jeremyahn.com>
2019-02-25 04:07:33 -0500

    comments

--- client.py
+++ client.py
@@ -15,25 +15,42 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
 	while True:
 		email = input('Enter an email: ')
 		if len(email) < 255 and is_valid_email(email):
-			#packer = struct.Struct('c c 255s')
+			# Set a dynamically-sized structure of size 2 + len(email)
 			packer = struct.Struct('c c ' + str(len(email)) + 's')
 			packed_data = packer.pack(b'Q', len(email).to_bytes(1, 'little'), email.encode('utf-8'))
 
+			# Send to server
 			sock.sendall(packed_data)
-			returned_data = sock.recv(257)
-			unpacked_data = packer.unpack(returned_data)
+
+			# Holds until hearing response to the server
+			# At first, we don't know the length of the returning message string after the two bytes,
+			# so we must read the second byte first to learn how many characters we must read after 
+			packer = struct.Struct('c c')
+			returned_data = sock.recv(256)
+			# [0:2] = b'R' + two char representation of the following byte
+			unpacked_data = packer.unpack(returned_data[0:2])
 
 			length_string = int.from_bytes(unpacked_data[1], byteorder='little')
+
+			# Now that we know the length of the string, redefine the packer structure and this time 
+			# get all three returned property values of returned_data
+			packer = struct.Struct('c c ' + str(length_string) + 's')
+			unpacked_data = packer.unpack(returned_data)
+
 			name = (unpacked_data[2][:length_string]).decode("utf-8")
 			if name == "Error":
 				print("No such name in directory!")
 			else:
 				print("Name:", name)
 		elif len(email) == 0:
+			# Empty input ends the client-sided service
 			break
 		elif len(email) >= 255:
+			# Client-sided character limit
 			print("Email input too long.")
 		elif not is_valid_email(email):
+			# We check if inputs are in valid email formats or not before bothering the server
 			print("Not a valid email")
 
+	# Close the socket when the while loop above is broken
 	sock.close()
--- server.py
+++ server.py
@@ -5,7 +5,7 @@ PORT = 420
 
 addressbook = {
 	"jeremy.ahn@stonybrook.edu": "Jeremy Ahn",
-	"arunab@cs.stonybrook.edu": "Aruna Balasubramanian",
+	"arunab@cs.stonybrook.edu": "Aruna Balasubramanianaaaaaaaaaaaaaaaaaaaaaaaa",
 	"steve@apple.com": "Steve Jobs",
 	"luke@gmail.com": "Luke Skywalker",
 }
@@ -17,15 +17,22 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
 	with conn:
 		print('Connected by ', addr)
 		while True:
-			received_data = conn.recv(257)
+			received_data = conn.recv(256)
 			if not received_data:
 				break
 
+			# We don't know the length of the email so only get the message type and string length
 			packer = struct.Struct('c c')
+
+			# At first, we don't know the length of the returning message string after the two bytes,
+			# so we must read the second byte first to learn how many characters we must read after 
+			# [0:2] = b'R' + two char representation of the following byte
 			unpacked_data = packer.unpack(received_data[0:2])
 
 			length_string = int.from_bytes(unpacked_data[1], byteorder='little')
 
+			# Now that we know the length of the string, redefine the packer structure and this time 
+			# get all three returned property values of returned_data
 			packer = struct.Struct('c c ' + str(length_string) + 's')
 			unpacked_data = packer.unpack(received_data)
 
@@ -33,10 +40,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
 
 			if email in addressbook:
 				contact = addressbook[email]
+				# Redefine packer structure once again to match size of the contact's name
+				packer = struct.Struct('c c ' + str(len(contact)) + 's')
 				packed_data = packer.pack(b'R', len(contact).to_bytes(1, 'little'), contact.encode('utf-8'))
 				conn.sendall(packed_data)
 			else:
 				ret = "Error"
+				# Redefine packer structure once again to match size of the "Error" string
+				packer = struct.Struct('c c ' + str(len(ret)) + 's')
 				packed_data = packer.pack(b'R', len(ret).to_bytes(1, 'little'), ret.encode('utf-8'))
 				conn.sendall(packed_data)
 
